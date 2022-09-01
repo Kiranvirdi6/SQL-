65ffeaaca9e5b5fcb5f3f64813b6a05f792dc466
kiran <Kiranvirdi6@gmail.com>
2022-08-31 20:11:43 -0400

    2022.08.21 - Adding empty basics.sql

---
+++ Basics.sql
@@ -0,0 +1,11 @@
+-- Learning Source : https://www.sqlservertutorial.net/sql-server-basics/sql-server-cte/
+
+-- Topic: Common Table Expression
+
+-- CTE Stands for Common Table expression
+    -- 1. Allows to create a temporary result set
+
+use Bikestores;
+select * from sales.orders;
+
+
